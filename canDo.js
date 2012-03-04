3b8ef11821fac97ddf4a73ab3da25d7b8d22aab0
Donovan Buck <donovan@donovan.bz>
2012-03-04 12:10:58 -0600

    switch to CanDo

--- canDo.js
+++ canDo.js
@@ -1,6 +1,6 @@
 /*global clearInterval: false, clearTimeout: false, document: false, event: false, frames: false, history: false, Image: false, location: false, name: false, navigator: false, Option: false, parent: false, screen: false, setInterval: false, setTimeout: false, window: false, XMLHttpRequest: false */
 
-var canDo = function (el, args) {
+var CanDo = function (el, args) {
 	"use strict";
 
 	// Our 2d rendering context
