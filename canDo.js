41075f10ee4e4c4dc636358dc1d2bfa8df072b96
Donovan Buck <donovan@donovan.bz>
2012-03-28 21:47:22 -0500

    Some linking. Added support for keyFrame.persist

--- canDo.js
+++ canDo.js
@@ -206,15 +206,59 @@ var CanDo = function (elid, args) { // this = Window
 				result.end = i + 1; // This could be our cuepoint (might be replaced later in our loop)
 			}
 		}
+
 		if (result.end === keyFrames.length) {
 			result.end = keyFrames.length - 1;
-			result.start = result.end - 1;
+			result.start = result.end;
 		}
 
 		result.subDuration = keyFrames[result.end].cuePoint - keyFrames[result.start].cuePoint; // Calculate the time between our two keyframes
 		result.subTime = (this.s.easedTime - keyFrames[result.start].cuePoint) / result.subDuration; // Calculate how far through this transition we are
 		return result;
 	};
+
+	// Parse strings looking for color tuples [255,255,255]
+	ctx.getRGB = function (color) {
+		var result;
+		// Check if we're already dealing with an array of colors
+		if ( color && color.constructor === Array && (color.length === 3 || color.length === 4 ) ) {
+			color.push(1);
+			return color;
+		}
+
+		// Look for rgba(num,num,num,num)
+		if (result = /rgba\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9.]{1,3})\s*\)/.exec(color)) {
+			return [parseInt(result[1],10), parseInt(result[2],10), parseInt(result[3],10), Number(result[4])];
+		}
+
+		// Look for rgb(num,num,num)
+		if (result = /rgb\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*\)/.exec(color)) {
+			return [parseInt(result[1],10), parseInt(result[2],10), parseInt(result[3],10), 1];
+		}
+
+		// Look for rgb(num%,num%,num%)
+		if (result = /rgb\(\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*\)/.exec(color)) {
+			return [parseFloat(result[1])*2.55, parseFloat(result[2])*2.55, parseFloat(result[3])*2.55, 1];
+		}
+
+		// Look for #a0b1c2
+		if (result = /#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})/.exec(color)) {
+				return [parseInt(result[1],16), parseInt(result[2],16), parseInt(result[3],16), 1];
+		}
+
+		// Look for #fff
+		if (result = /#([a-fA-F0-9])([a-fA-F0-9])([a-fA-F0-9])/.exec(color)) {
+				return [parseInt(result[1]+result[1],16), parseInt(result[2]+result[2],16), parseInt(result[3]+result[3],16), 1];
+		}
+
+
+		// Look for rgba(0, 0, 0, 0) == transparent in Safari 3
+		if (result = /rgba\(0, 0, 0, 0\)/.exec(color))
+				return colors["transparent"];
+
+		// Otherwise, we're most likely dealing with a named color
+		return colors[color.toLowerCase()];
+	}
 
 	// Convenience method for resetting the transformation matrix to the identity transform
 	ctx.identity = function () {
@@ -223,35 +267,48 @@ var CanDo = function (elid, args) { // this = Window
 
 	// Here's our method wrapper
 	ctx.canDo = function (method, keyFrames) {
-		var beg = this.getCurrentKeyframe(keyFrames), // Find the start point for the current animation segment
-			state = ctx.easeThis(keyFrames[beg.end].easing || 'linear', beg.subTime, keyFrames[beg.start].params, keyFrames[beg.end].params), // Eases all the properties in the array
+		var state, result, beg = this.getCurrentKeyframe(keyFrames); // Find the start point for the current animation segment
+		if (beg.start === beg.end) {
+			state = keyFrames[beg.start].params;
+		} else {
+			state = ctx.easeThis(keyFrames[beg.end].easing || 'linear', beg.subTime, keyFrames[beg.start].params, keyFrames[beg.end].params); // Eases all the properties in the array
+		}
+		if ( ctx.s.time > .95 ) console.log(method, beg.start != beg.end, beg.end.cuePoint == 1.0, beg.end.cuePoint !== 1.0, beg.end.persist === true);
+		if (beg.start != beg.end || keyFrames[beg.end].cuePoint === 1.0 ||(keyFrames[beg.end].cuePoint !== 1.0 && keyFrames[beg.end].persist === true)) {
 			result = this[method].apply(this, state); // Call the proxied function with the computed parameters
+		}
 		return result;
 	};
 
 	// Here's our setter wrapper
 	ctx.canSet = function (property, keyFrames) {
-		var beg = this.getCurrentKeyframe(keyFrames), // Find the start point for the current animation segment
+		var state, result, beg = this.getCurrentKeyframe(keyFrames); // Find the start point for the current animation segment
+		if (beg.start === beg.end) {
+			state = keyFrames[beg.start].params;
+		} else {
 			state = ctx.easeThis(keyFrames[beg.end].easing || 'linear', beg.subTime, keyFrames[beg.start].params, keyFrames[beg.end].params); // Eases all the properties in the array
-		this[property] = state[0]; // Call the proxied function with the computed parameters
+		}
+		if (beg.start != beg.end || beg.end.cuePoint !== 1.0) {
+			this[property] = state[0]; // Call the proxied function with the computed parameters
+		}
 	};
 
 	// This is our easing wrapper
 	ctx.easeThis = function (easing, time, startParams, endParams) {
 
-		var i,  j = startParams.length, result = [], state;
+		var i,  j = startParams.length, result = [], state, startColor, endColor, stateColor;
 		for (i = 0; i < j; i = i + 1) { // Loop through all the parameters in the keyFrames
 
 			if (!isNaN(parseFloat(startParams[i])) && isFinite(startParams[i])) { // This is a number so use normal easing
-				
+
 				state = ctx.easing[easing](time); // Get the eased subTime
 				result.push((endParams[i] - startParams[i]) * state + startParams[i]); // Add the eased value to the results array
-			
-			} else if (typeof startParams[i] === 'string' && startParams[i].substring(0,3) == 'rgb') {  // This is a color
-				var startColor = getRGB(startParams[i]),
-					endColor = getRGB(endParams[i]),
+
+			} else if (typeof startParams[i] === 'string' && (startParams[i].substring(0, 3) === 'rgb' ||  startParams[i].substring(0, 1) === '#')) {  // This is a color
+				startColor = ctx.getRGB(startParams[i]),
+					endColor = ctx.getRGB(endParams[i]),
 					stateColor = ctx.easeThis(easing, time, startColor, endColor);
-				result.push('rgba(' + Math.floor(Number(stateColor[0])) + ',' +Math.floor(Number(stateColor[1])) + ',' + Math.floor(Number(stateColor[2])) + ',' + Number(stateColor[3]) + ')');
+				result.push('rgba(' + Math.floor(Number(stateColor[0])) + ',' + Math.floor(Number(stateColor[1])) + ',' + Math.floor(Number(stateColor[2])) + ',' + Number(stateColor[3]) + ')');
 
 			} else { // This must be a string
 				result.push(startParams[i]); // Pass in the value from the start keyframe
@@ -282,96 +339,58 @@ var CanDo = function (elid, args) { // this = Window
 			}
 		}
 	};
-	
+
 	// Color Conversion functions from highlightFade
 	// By Blair Mitchelmore
 	// http://jquery.offput.ca/highlightFade/
-	
+
 	var colors = {
-		//aqua:[0,255,255,1],
-		//azure:[240,255,255,1],
-		//beige:[245,245,220,1],
-		black:[0,0,0,1],
-		blue:[0,0,255,1],
-		//brown:[165,42,42,1],
-		//cyan:[0,255,255,1],
-		//darkblue:[0,0,139,1],
-		//darkcyan:[0,139,139,1],
-		//darkgrey:[169,169,169,1],
-		//darkgreen:[0,100,0,1],
-		//darkkhaki:[189,183,107,1],
-		//darkmagenta:[139,0,139,1],
-		//darkolivegreen:[85,107,47,1],
-		//darkorange:[255,140,0,1],
-		//darkorchid:[153,50,204,1],
-		//darkred:[139,0,0,1],
-		//darksalmon:[233,150,122,1],
-		//darkviolet:[148,0,211,1],
-		//fuchsia:[255,0,255,1],
-		//gold:[255,215,0,1],
-		green:[0,128,0,1],
-		//indigo:[75,0,130,1],
-		//khaki:[240,230,140,1],
-		//lightblue:[173,216,230,1],
-		//lightcyan:[224,255,255,1],
-		//lightgreen:[144,238,144,1],
-		//lightgrey:[211,211,211,1],
-		//lightpink:[255,182,193,1],
-		//lightyellow:[255,255,224,1],
-		//lime:[0,255,0,1],
-		//magenta:[255,0,255,1],
-		//maroon:[128,0,0,1],
-		//navy:[0,0,128,1],
-		//olive:[128,128,0,1],
-		//orange:[255,165,0,1],
-		//pink:[255,192,203,1],
-		//purple:[128,0,128,1],
-		//violet:[128,0,128,1],
-		red:[255,0,0,1],
-		//silver:[192,192,192,1],
-		white:[255,255,255,1],
-		//yellow:[255,255,0,1],
-		transparent: [0,0,0,0]
+		//aqua: [0, 255, 255, 1],
+		//azure: [240, 255, 255, 1],
+		//beige: [245, 245, 220, 1],
+		black: [0, 0, 0, 1],
+		blue: [0, 0, 255, 1],
+		//brown: [165, 42, 42, 1],
+		//cyan: [0, 255, 255, 1],
+		//darkblue: [0, 0, 139, 1],
+		//darkcyan: [0, 139, 139, 1],
+		//darkgrey: [169, 169, 169, 1],
+		//darkgreen: [0, 100, 0, 1],
+		//darkkhaki: [189, 183, 107, 1],
+		//darkmagenta: [139, 0, 139, 1],
+		//darkolivegreen: [85, 107, 47, 1],
+		//darkorange: [255, 140, 0, 1],
+		//darkorchid: [153, 50, 204, 1],
+		//darkred: [139, 0, 0, 1],
+		//darksalmon: [233, 150, 122, 1],
+		//darkviolet: [148, 0, 211, 1],
+		//fuchsia: [255, 0, 255, 1],
+		//gold: [255, 215, 0, 1],
+		green: [0, 128, 0, 1],
+		//indigo: [75, 0, 130, 1],
+		//khaki: [240, 230, 140, 1],
+		//lightblue: [173, 216, 230, 1],
+		//lightcyan: [224, 255, 255, 1],
+		//lightgreen: [144, 238, 144, 1],
+		//lightgrey: [211, 211, 211, 1],
+		//lightpink: [255, 182, 193, 1],
+		//lightyellow: [255, 255, 224, 1],
+		//lime: [0, 255, 0, 1],
+		//magenta: [255, 0, 255, 1],
+		//maroon: [128, 0, 0, 1],
+		//navy: [0, 0, 128, 1],
+		//olive: [128, 128, 0, 1],
+		//orange: [255, 165, 0, 1],
+		//pink: [255, 192, 203, 1],
+		//purple: [128, 0, 128, 1],
+		//violet: [128, 0, 128, 1],
+		red: [255, 0, 0, 1],
+		//silver: [192, 192, 192, 1],
+		white: [255, 255, 255, 1],
+		//yellow: [255, 255, 0, 1],
+		transparent: [0, 0, 0, 0]
 	};
-	
-	// Parse strings looking for color tuples [255,255,255]
-	function getRGB(color) {
-		var result;
-		// Check if we're already dealing with an array of colors
-		if ( color && color.constructor === Array && color.length === 3 ) {
-				color.push(1);
-				return color;
-		}
 
-		// Look for rgba(num,num,num,num)
-		if (result = /rgba\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9.]{1,3})\s*\)/.exec(color))
-				return [parseInt(result[1],10), parseInt(result[2],10), parseInt(result[3],10), Number(result[4])];
-				
-		// Look for rgb(num,num,num)
-		if (result = /rgb\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*\)/.exec(color))
-				return [parseInt(result[1],10), parseInt(result[2],10), parseInt(result[3],10), 1];
-
-		// Look for rgb(num%,num%,num%)
-		if (result = /rgb\(\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*\)/.exec(color))
-				return [parseFloat(result[1])*2.55, parseFloat(result[2])*2.55, parseFloat(result[3])*2.55, 1];
-		
-		// Look for #a0b1c2
-		if (result = /#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})/.exec(color))
-				return [parseInt(result[1],16), parseInt(result[2],16), parseInt(result[3],16), 1];
-
-		// Look for #fff
-		if (result = /#([a-fA-F0-9])([a-fA-F0-9])([a-fA-F0-9])/.exec(color))
-				return [parseInt(result[1]+result[1],16), parseInt(result[2]+result[2],16), parseInt(result[3]+result[3],16), 1];
-
-		
-				
-	// Look for rgba(0, 0, 0, 0) == transparent in Safari 3
-		if (result = /rgba\(0, 0, 0, 0\)/.exec(color))
-				return colors["transparent"];
-
-		// Otherwise, we're most likely dealing with a named color
-		return colors[color.toLowerCase()];
-}
 
 	/******************************************************************************/
 	/*********************************** EASING ***********************************/
