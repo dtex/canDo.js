3fce45a69e296488c407a5c37ddd71f1e404bf49
Donovan Buck <donovan@donovan.bz>
2012-04-02 21:17:21 -0500

    Added some comments

--- canDo.js
+++ canDo.js
@@ -24,6 +24,9 @@ http://jquery.offput.ca/highlightFade/
 
 Easing equations from Robert Penner (http://www.robertpenner.com/easing)
 Blatantly lifted from jQuery UI
+
+Stripped down implementation of each inspired by jQuery
+
 */
 
 /*global clearInterval: false, clearTimeout: false, document: false, event: false, frames: false, history: false, Image: false, location: false, name: false, navigator: false, Option: false, parent: false, screen: false, setInterval: false, setTimeout: false, window: false, XMLHttpRequest: false */
@@ -36,6 +39,7 @@ var CanDo = function (elid, args) { // this = Window
 	"use strict";
 	// Hoisted variables
 	var el = document.getElementById(elid), ctx, baseEasings, imagesLeftToLoad = 0, colors = { black: [0, 0, 0, 1], blue: [0, 0, 255, 1], green: [0, 128, 0, 1], red: [255, 0, 0, 1], white: [255, 255, 255, 1], transparent: [0, 0, 0, 0]},
+		/* More color names we could add */
 		/*aqua: [0, 255, 255, 1], azure: [240, 255, 255, 1], beige: [245, 245, 220, 1], brown: [165, 42, 42, 1], cyan: [0, 255, 255, 1], darkblue: [0, 0, 139, 1], darkcyan: [0, 139, 139, 1], darkgrey: [169, 169, 169, 1], darkgreen: [0, 100, 0, 1], darkkhaki: [189, 183, 107, 1], darkmagenta: [139, 0, 139, 1], darkolivegreen: [85, 107, 47, 1], darkorange: [255, 140, 0, 1], darkorchid: [153, 50, 204, 1], darkred: [139, 0, 0, 1], darksalmon: [233, 150, 122, 1], darkviolet: [148, 0, 211, 1], fuchsia: [255, 0, 255, 1], gold: [255, 215, 0, 1], indigo: [75, 0, 130, 1], khaki: [240, 230, 140, 1], lightblue: [173, 216, 230, 1], lightcyan: [224, 255, 255, 1], lightgreen: [144, 238, 144, 1], lightgrey: [211, 211, 211, 1], lightpink: [255, 182, 193, 1], lightyellow: [255, 255, 224, 1], lime: [0, 255, 0, 1], magenta: [255, 0, 255, 1], maroon: [128, 0, 0, 1], navy: [0, 0, 128, 1], olive: [128, 128, 0, 1], orange: [255, 165, 0, 1], pink: [255, 192, 203, 1], purple: [128, 0, 128, 1], violet: [128, 0, 128, 1], silver: [192, 192, 192, 1], yellow: [255, 255, 0, 1],*/
 		imageLoaded = function () {// Called when an image is loaded
 			imagesLeftToLoad = imagesLeftToLoad - 1;
@@ -126,7 +130,7 @@ var CanDo = function (elid, args) { // this = Window
 
 		// Event handlers
 		if (typeof args.events !== 'undefined') { // If an events object was passed
-			for (eventName in	 args.events) { // Loop through each event
+			for (eventName in args.events) { // Loop through each event
 				if (args.events.hasOwnProperty(eventName)) {
 					if (el.addEventListener) {
 						el.addEventListener(eventName, args.events[eventName], false);
